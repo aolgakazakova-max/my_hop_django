5c7d08f3a360e9781246114391375e3010f1711b
kgele <a.olga.kazakova@gmail.com>
2026-09-22 13:07:01 +0500

    Configure CI with PostgreSQL

---
+++ config/settings/ci.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa: F403
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'hop_django_luxe'),
+        'USER': os.environ.get('POSTGRES_USER', 'hop_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'hop_password'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    }
+}
